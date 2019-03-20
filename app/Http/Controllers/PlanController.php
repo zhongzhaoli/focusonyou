30257a43a818a94a143897fd5c7ed889f7d2e365
钟兆立 <525966315@qq.com>
2019-03-20 10:51:21 +0800

    plan store

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -16,4 +16,24 @@ class PlanController extends Controller
             return response()->json(["message" => "null"], 400);
         }
     }
+    public function store(Request $request){
+        $text = $request->get("content");
+        if($text == ""){
+            return response()->json(["message" => "计划不能为空哦！"], 400);
+        }
+        $user_id = $request->get("userid");
+        $bg = 'bg' . rand(1, 4);
+        $id = time() . md5(uniqid());
+        $create_time = date("Y-m-d H:i:s");
+        $a = DB::table("plan")->insert([
+            "id" => $id,
+            "user_id" => $user_id,
+            "text" => $text,
+            "create_time" => $create_time,
+            "bg" => $bg
+        ]);
+        if($a){
+            return response()->json(["message" => "提交成功"], 200);
+        }
+    }
 }
