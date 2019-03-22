24bbd6db31ba13facf2ef9171d05f01c05ae8469
钟兆立 <525966315@qq.com>
2019-03-22 11:23:49 +0800

    打卡

--- app/Http/Controllers/SleepController.php
+++ app/Http/Controllers/SleepController.php
@@ -3,28 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class SleepController extends Controller
 {
     public function store(Request $request){
         $now_hour = date("H");
-        if($now_hour > 20 && $now_hour < 23){
+        if($now_hour > 10 && $now_hour < 23){
             $user_id = $request->get("user_id");            
-            $create_time = date("Y-m-d H:i:s");
-            $create_time_data = date("Y-m-d");
-            $clock_time = date("H:i:s");
-            $a = DB::table("sleep")->insert([
-                "id" => time() . md5(uniqid()),
-                "user_id" => $user_id,
-                "create_time" => $create_time,
-                "create_time_data" => $create_time_data,
-                "clock_time" => $clock_time
-            ]);
-            if($a){
-                return response()->json(["message" => "打卡成功", "status" => "200"], 200);                
+            $b = DB::table("sleep")->where(["user_id" => $user_id, "create_time_data" => date("Y-m-d")])->get();
+            if(!count($b)){
+                $create_time = date("Y-m-d H:i:s");
+                $create_time_data = date("Y-m-d");
+                $clock_time = date("H:i:s");
+                $a = DB::table("sleep")->insert([
+                    "id" => time() . md5(uniqid()),
+                    "user_id" => $user_id,
+                    "create_time" => $create_time,
+                    "create_time_data" => $create_time_data,
+                    "clock_time" => $clock_time
+                ]);    
+                if($a){
+                    return response()->json(["message" => "打卡成功", "status" => "200"], 200);                
+                }
+                else{
+                    return response()->json(["message" => "打卡失败", "status" => "400"], 200);                
+                }
             }
             else{
-                return response()->json(["message" => "打卡失败", "status" => "400"], 200);                
+                return response()->json(["message" => "今天已打卡", "status" => "400"], 200);                                
             }
         }
         else{
