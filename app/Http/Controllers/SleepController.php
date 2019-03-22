6ac4fec0954700a214f9a0b18b2699a19a1c943d
钟兆立 <525966315@qq.com>
2019-03-22 14:59:49 +0800

    连续打卡

--- app/Http/Controllers/SleepController.php
+++ app/Http/Controllers/SleepController.php
@@ -7,20 +7,57 @@ use DB;
 
 class SleepController extends Controller
 {
+    public function show($user_id){
+        //返回 今天要不要打卡，连续打卡，累计打卡，最长连续打卡
+        $return_data = [
+            //0是不用打卡 1是用打卡
+            "need_clock" => 1,
+            "clock_count" => 0,
+            "continuity_clock" => 0
+        ];
+
+        //获取user的所有打卡纪录  [1,2,3,4] count = 4
+        $a = DB::table("sleep")->where("user_id", $user_id)->OrderBy("create_time_date","desc")->get();
+        //今天
+        $today = date("Y-m-d");
+        if(count($a)){
+            if($a[0]->create_time_date == date("Y-m-d")){
+                $return_data['need_clock'] = 0;
+            }
+            else{
+                $return_data['need_clock'] = 1;
+            }
+            $return_data['clock_count'] = count($a);
+
+            //连续打卡 先获取今天和数据库的第一条是否相差1 如果不是 就默认是0了
+            $sql_first = (date('Y-m-d',strtotime(date('Y-m-d')) - 3600 * 24) == $a[0]->create_time_date) ? 'true' : 'false';
+
+            for($i = count($a); $i > 0; $i--){
+                $j = $i - 1;
+                //如果就相差一天 也就是说昨天也打卡了
+                if($sql_first && $i > 1){
+                //     //判断此次和下次是否相差一天 是的话就+1
+                    if(date("Y-m-d",strtotime($a[$j]->create_time_date) - 3600 * 24) == date("Y-m-d",strtotime($a[$j - 1]->create_time_date))){
+                    }
+                }
+            }
+        }
+        return $return_data;
+    }
     public function store(Request $request){
         $now_hour = date("H");
         if($now_hour > 10 && $now_hour < 23){
             $user_id = $request->get("user_id");            
-            $b = DB::table("sleep")->where(["user_id" => $user_id, "create_time_data" => date("Y-m-d")])->get();
+            $b = DB::table("sleep")->where(["user_id" => $user_id, "create_time_date" => date("Y-m-d")])->get();
             if(!count($b)){
                 $create_time = date("Y-m-d H:i:s");
-                $create_time_data = date("Y-m-d");
+                $create_time_date = date("Y-m-d");
                 $clock_time = date("H:i:s");
                 $a = DB::table("sleep")->insert([
                     "id" => time() . md5(uniqid()),
                     "user_id" => $user_id,
                     "create_time" => $create_time,
-                    "create_time_data" => $create_time_data,
+                    "create_time_date" => $create_time_date,
                     "clock_time" => $clock_time
                 ]);    
                 if($a){
