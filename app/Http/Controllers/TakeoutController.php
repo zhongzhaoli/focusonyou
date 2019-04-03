7337a96b849b774542a8aa8de1229287e94f9ee2
Custer <525966315@qq.com>
2019-04-04 00:41:35 +0800

    发布商家

---
+++ app/Http/Controllers/TakeoutController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use DB;
+
+class TakeoutController extends Controller
+{
+    public function store(Request $request){
+        $img_arr = [$request->get("cover"), $request->get("menu")];
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'phone' => 'required|regex:/^1[345789][0-9]{9}$/',
+            'cover' => "required",
+            'menu' => "required"
+        ],[
+            'name.required' => "商家名不能为空",
+            'start_time.required' => "外卖开始时间不能为空",
+            'end_time.required' => "外卖结束时间不能为空",
+            'phone.required' => "外卖电话不能为空",
+            'phone.regex' => "外卖电话不合法",
+            'cover.required' => "封面图不能为空",
+            'menu.required' => "菜单图片不能为空",
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);            
+        }
+        $cover = "";
+        $menu = "";
+        for($i = 0; $i < count($img_arr); $i++){
+            $prove_up = new ProveUpload();
+            $bo_prove = $prove_up->upload($img_arr[$i],"uploads/");
+            if(!$bo_prove){
+                return response()->json(["prove" => "图片有错"],400);
+            }
+            $prove_url = "http://localhost:7889/".$bo_prove;
+            ($i == 0) ? $cover = $prove_url : $menu = $prove_url;
+            $request["prove"] = $prove_url;
+        }
+
+        $a = DB::table("takeout")->insert([
+            "id" => time() . md5(uniqid()),
+            "name" => $request->get("name"),
+            "phone" => $request->get("phone"),
+            "start_time" => $request->get("start_time"),
+            "end_time" => $request->get("end_time"),
+            "create_time" => date("Y-m-d H:i:s"),
+            "cover" => $cover,
+            "menu" => $menu
+        ]);
+        if($a){
+            return response()->json(["message" => ["提交成功"]],200);
+        }
+        else{
+            return response()->json(["message" => ["提交失败"]],400);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,6 @@ Route::post('register', 'PassportController@register');
 
 // ----------------------------------管理员用户权限---------------------------------------
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::get("admin",function(){
-        return "da";
-    });
     Route::get("appeal/{table_name}", "AppealController@show");
+    Route::post("/takeout", "TakeoutController@store");
 });
